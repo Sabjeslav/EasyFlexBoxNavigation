0eca4df0a5e35993575ba8633f14dbe98f392966
Sabjeslav <s4tenko@gmail.com>
2021-03-09 09:49:07 +0200

    Small script changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
 window.addEventListener('DOMContentLoaded', () => {
   const navbar = document.querySelector('.navbar-content'),
   navbarItem = document.querySelectorAll('.navbar-content-item'),
-  hamburger = document.querySelector('.hamburger');
+  hamburger = document.querySelector('.hamburger'),
+  main = document.getElementById('main');
+
+  toggleBurger = () => {
+    hamburger.classList.toggle('hamburger-active');
+    navbar.classList.toggle('navbar-active');
+  };
 
   hamburger.addEventListener('click', () => {
-      hamburger.classList.toggle('hamburger-active');
-      navbar.classList.toggle('navbar-active');
+      toggleBurger();
   });
 
   navbarItem.forEach(item => {
       item.addEventListener('click', () => {
-          hamburger.classList.toggle('hamburger-active');
-          navbar.classList.toggle('navbar-active');
+        toggleBurger();
       })
   })
 })
