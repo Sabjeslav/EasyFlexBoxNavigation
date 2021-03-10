a331e961e77d161c0309245df5a2bb69fc498aff
Sabjeslav <s4tenko@gmail.com>
2021-03-10 08:46:24 +0200

    Useless varible removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ window.addEventListener('DOMContentLoaded', () => {
   const navbar = document.querySelector('.navbar-content'),
   navbarItem = document.querySelectorAll('.navbar-content-item'),
   hamburger = document.querySelector('.hamburger'),
-  main = document.getElementById('main');
 
   toggleBurger = () => {
     hamburger.classList.toggle('hamburger-active');
